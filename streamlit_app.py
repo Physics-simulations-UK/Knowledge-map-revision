43ef769a036a6e49e0e1bc73d1118635b5f83ed4
Physics-simulations-UK <dhrayner@gmail.com>
2026-04-26 10:51:36 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_agraph import agraph, Node, Edge, Config
-st.sest.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 from google import genai
 from google.genai import types
 import time
